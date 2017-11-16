165056c9debe69cdd36f1bf95657a5c6b3f2c7da
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 11:49:24 +0700

    Add Exercise04

---
+++ Exercise04/Rect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04
+{
+    class Rect
+    {
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public Rect(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+        public int GetPerimeter()
+        {
+            return (Width + Height) * 2;
+        }
+        public int GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
