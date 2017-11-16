00ca3e4f860c1137b37b37e8b674c5d6d957a43e
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 10:56:15 +0700

    Add Exercise02

---
+++ Exercise02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Size of list: ");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            var list = new List<Student>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Birthday(dd/mm/yyyy): ");
+                var str = Console.ReadLine();
+                Console.WriteLine();
+                var arr = str.Split('/');
+                var day = int.Parse(arr[0]);
+                var month = int.Parse(arr[1]);
+                var year = int.Parse(arr[2]);
+                var birthday = new DateTime(year, month, day);
+                Console.Write("Score: ");
+                var score = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                list.Add(new Student(name, birthday, score));
+            }
+            list = list.OrderBy(x => x.Score).ToList();
+            foreach(var stu in list)
+            {
+                Console.WriteLine(stu.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise02/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Student
+    {
+        public string Name { set; get; }
+        public DateTime Birthday { set; get; }
+        public double Score { set; get; }
+        public Student(string Name,DateTime Birthday,double Score)
+        {
+            this.Name = Name;
+            this.Birthday = Birthday;
+            this.Score = Score;
+        }
+        public override string ToString()
+        {
+            return this.Name + " " + this.Birthday.ToString("dd'/'mm'/'yyyy") + " " + this.Score;
+        }
+    }
+}
