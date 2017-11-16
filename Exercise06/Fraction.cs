ebbf21d436805b66646d58e852308ad46dd845cd
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 15:29:14 +0700

    Add Exercise06

---
+++ Exercise06/Fraction.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    class Fraction
+    {
+        public int a { set; get; }
+        public int b { set; get; }
+
+        public Fraction(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        
+        public void Minimal()
+        {
+            var gcd = GCD(a, b);
+            a = a / gcd;
+            b = b / gcd;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+        public static Fraction operator +(Fraction q, Fraction w)
+        {
+            var e = new Fraction(q.a * w.b + q.b * w.a, q.b * w.b);
+            e.Minimal();
+            return e;
+        }
+
+        public static Fraction operator -(Fraction q, Fraction w)
+        {
+            var e = new Fraction(q.a * w.b - q.b * w.a, q.b * w.b);
+            e.Minimal();
+            return e;
+        }
+
+        public static Fraction operator *(Fraction q, Fraction w)
+        {
+            var e = new Fraction(q.a * w.a, q.b * w.b);
+            e.Minimal();
+            return e;
+        }
+
+        public static Fraction operator /(Fraction q, Fraction w)
+        {
+            var e = new Fraction(q.a * w.b, q.b * w.a);
+            e.Minimal();
+            return e;
+        }
+
+        public static bool operator !=(Fraction q, Fraction w)
+        {
+            q.Minimal();
+            w.Minimal();
+            return (q.a != w.a || q.b != w.b);
+        }
+
+        public static bool operator ==(Fraction q, Fraction w)
+        {
+            return !(q!=w);
+        }
+
+        public static bool operator >=(Fraction q, Fraction w)
+        {
+            return (q.a * w.b >= q.b * w.a);
+        }
+
+        public static bool operator >(Fraction q, Fraction w)
+        {
+            return (q.a * w.b > q.b * w.a);
+        }
+
+        public static bool operator <=(Fraction q, Fraction w)
+        {
+            return (q.a * w.b <= q.b * w.a);
+        }
+
+        public static bool operator <(Fraction q, Fraction w)
+        {
+            return (q.a * w.b < q.b * w.a);
+        }
+
+        public static Fraction operator !(Fraction q)
+        {
+            return new Fraction(q.b, q.a);
+        }
+
+        public int GCD(int q, int w)
+        {
+            while (q * w != 0) 
+            {
+                if (q > w)
+                {
+                    q = q % w;
+                } else
+                {
+                    w = w % q;
+                }
+            }
+            return q + w;
+        }
+    }
+}
---
+++ Exercise06/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fra = new Fraction(4, 6);
+            Console.WriteLine(fra.ToString());
+            fra.Minimal();
+            Console.WriteLine("Minimal: " + fra.ToString());
+            var fra1 = new Fraction(2, 3);
+            var fra2 = new Fraction(2, 4);
+            fra = fra1 + fra2;
+            Console.WriteLine("2/3 + 2/4: " + fra.ToString());
+            fra = fra1 - fra2;
+            Console.WriteLine("2/3 - 2/4: " + fra.ToString());
+            fra = fra1 * fra2;
+            Console.WriteLine("2/3 * 2/4: " + fra.ToString());
+            fra = fra1 / fra2;
+            Console.WriteLine("2/3 / 2/4: " + fra.ToString());
+            fra = !fra2;
+            Console.WriteLine("!2/4: " + fra.ToString());
+            fra1 = new Fraction(2, 3);
+            fra2 = new Fraction(2, 4);
+            Console.WriteLine("2/3 compare 2/4");
+            if (fra1 >= fra2) 
+            {
+                Console.WriteLine(">=");
+            }
+            if (fra1 > fra2)
+            {
+                Console.WriteLine(">");
+            }
+            if (fra1 <= fra2)
+            {
+                Console.WriteLine("<=");
+            }
+            if (fra1 < fra2)
+            {
+                Console.WriteLine("<");
+            }
+            fra1 = new Fraction(2, 3);
+            fra2 = new Fraction(2, 3);
+            Console.WriteLine("2/3 compare 2/3");
+            if (fra1 >= fra2)
+            {
+                Console.WriteLine(">=");
+            }
+            if (fra1 > fra2)
+            {
+                Console.WriteLine(">");
+            }
+            if (fra1 <= fra2)
+            {
+                Console.WriteLine("<=");
+            }
+            if (fra1 < fra2)
+            {
+                Console.WriteLine("<");
+            }
+            fra1 = new Fraction(2, 4);
+            fra2 = new Fraction(2, 3);
+            Console.WriteLine("2/4 compare 2/3");
+            if (fra1 >= fra2)
+            {
+                Console.WriteLine(">=");
+            }
+            if (fra1 > fra2)
+            {
+                Console.WriteLine(">");
+            }
+            if (fra1 <= fra2)
+            {
+                Console.WriteLine("<=");
+            }
+            if (fra1 < fra2)
+            {
+                Console.WriteLine("<");
+            }
+            Console.ReadKey();
+        }
+    }
+}
