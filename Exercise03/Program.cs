14ec19c56acb8aac406798f9f2847df78c913643
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-16 11:29:33 +0700

    Add Exercise03

---
+++ Exercise03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tri = new Triangle(2, 3, 4);
+            Console.WriteLine(tri.GetTriangleType());
+            Console.WriteLine("Perimeter: " + tri.GetPerimeter());
+            Console.WriteLine("Area: " + tri.GetArea());
+            tri = new Triangle(2, 2, 3);
+            Console.WriteLine(tri.GetTriangleType());
+            Console.WriteLine("Perimeter: " + tri.GetPerimeter());
+            Console.WriteLine("Area: " + tri.GetArea());
+            tri = new Triangle(3, 3, 3);
+            Console.WriteLine(tri.GetTriangleType());
+            Console.WriteLine("Perimeter: " + tri.GetPerimeter());
+            Console.WriteLine("Area: " + tri.GetArea());
+            tri = new Triangle(3, 4, 5);
+            Console.WriteLine(tri.GetTriangleType());
+            Console.WriteLine("Perimeter: " + tri.GetPerimeter());
+            Console.WriteLine("Area: " + tri.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise03/Triangle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Triangle
+    {
+        public int q { set; get; }
+        public int w { set; get; }
+        public int e { set; get; }
+        public Triangle(int q, int w, int e)
+        {
+            this.q = q;
+            this.w = w;
+            this.e = e;
+        }
+        public String GetTriangleType()
+        {
+            var str = "Type: ";
+            if (q + w > e && w + e > q && e + q > w)
+            {
+                int kt = 0;
+                if (q == w && w == e)
+                {
+                    str = str + "Equilateral Triangle";
+                    kt = 1;
+                }
+                else
+                {
+                    if (q == w || w == e || e == q)
+                    {
+                        str = str + "Isosceles Triangle";
+                        kt = 1;
+                    }
+                    if (q * q + w * w == e * e || w * w + e * e == q * q || e * e + q * q == w * w)
+                    {
+                        str = str + "Square Triangle";
+                        kt = 1;
+                    }
+                }
+                if (kt == 0)
+                {
+                    str = str + "Normal Triangle";
+                }
+                
+                
+            }
+            else
+            {
+                str = str + "Not Triangle";
+            }
+            return str;
+        }
+        public int GetPerimeter()
+        {
+            if (GetTriangleType().Equals("Not Triangle"))
+            {
+                return 0;
+            }
+            var perimeter = q + w + e;
+            return perimeter;
+        }
+        public double GetArea()
+        {
+            if (GetTriangleType().Equals("Not Triangle"))
+            {
+                return 0;
+            }
+            var area = Math.Sqrt((q + w + e) * (q + w - e) * (w + e - q) * (e + q - w)) / 4;
+            return area;
+        }
+    }
+}
